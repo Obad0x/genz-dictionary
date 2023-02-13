a14de7ccc9e906f51fc0179221550b8c2a14e148
SIta Dabo <euphoria214rue@gmail.com>
2023-02-13 23:46:36 +0100

    major updates
    added the emoji function so it is now complete

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
+ const express = require('express');
+ const Emojis = require ('./Emojifacts')
+ const app = express();
+const port = process.env.PORT;
+
+app.set('view engine', 'ejs');
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
 
@@ -7,15 +15,48 @@ app.get('/', (req, res) => {
 })
 
 app.get('/emoji', (req, res) => {
-  res.render('emoji')
+  res.render('emoji', { title : "emoji"})
 })
 
+
+// POST request searching for the emoji
 app.post('/emoji', (req, res) => {
 
 
+  // storing the search query  in an emoji variable
+  let emoji = req.body.emoji
+
+  // declared a variable for the result or the output
+
+  let result;
+
+  // using a foreach loop to loop through checking if the emoji eexist
+Emojis.Emojifacts.forEach(element => {
+      if(element.emoji === emoji){
+        result = element
+      }
+});
+
+if (result){
+  return res.render('emoji', { title : "emoji" , data : result})
+}
+
+return res.status(404).json({
+  sucess : false, 
+  data : `No data exist for this emoji ${emoji}`
+})
+
+});
+
+
+
+app.get('slangs', (req, res)=>{
+  res.send('slangs')
 })
 
 
-app.get('/slangs', (req, res) => {
-  res.render('slangs');
+ app.listen(port, (req, res)=>{
+  console.log(`gen-z dictionary is live on the server on port ${port}`)
 })
+
+console.log(port)
